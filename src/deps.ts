04ab755c6d00dba26bf2dd7d50626176d4d35dd4
i-xi-dev <naoyuki.isono@gmail.com>
2022-12-17 09:36:24 +0900

    1.0.3

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { Integer } from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.1.1/mod.ts";
-export { _ProgressEvent } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.1.2/mod.ts";
+export { Integer } from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.1.2/mod.ts";
+export { _ProgressEvent } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.1.3/mod.ts";
