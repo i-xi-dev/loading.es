3d4b823e9c41b17f283a4aee2bdc7506cd958617
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:03:09 +0900

    1.0.24

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { _ProgressEvent } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.15/mod.ts";
-export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
+export { _ProgressEvent } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.16/mod.ts";
+export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
